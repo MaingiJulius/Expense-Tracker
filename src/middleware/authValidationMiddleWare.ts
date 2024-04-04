20721e53fedda693479bc252219c73cb05f98de7
MaingiJulius <164306658+MaingiJulius@users.noreply.github.com>
2024-04-04 11:31:44 +0300

    Changes by emwaura

--- src/middleware/authValidationMiddleWare.ts
+++ src/middleware/authValidationMiddleWare.ts
@@ -14,7 +14,44 @@ export function validateLogin():ValidationChain[] {
             .withMessage("Password Must be at least 8 characters long.")
             .matches(/\d/)
             .withMessage("Password must contain at least one digit.")
+            .matches(/[!@#$%^(),.":{}|]/)
+            .withMessage('Password must contain at least one special character.')
             .trim(),
+    ];
+}
 
+/**
+ * Validate account request fields.
+ * @see {@link https://www.npmjs.com/package/express-validator}
+ * @see {@link https://express-validator.github.io/docs}
+ * @returns {ValidationChain[]} An array of express-validator chains for validating the account creation request.
+ */
+export function validateAccountCreation():ValidationChain[] {
+    return [
+        body ("email")
+            .isEmail()
+            .withMessage("Invalid Email Address")
+            .normalizeEmail(),
+
+
+        body("firstName")
+        .isLength({min:3,max:20})
+        .withMessage("First Name is required.")
+         .matches(/^[a-zA-Z]+$/),
+
+        body("lastName")
+            .isLength({min:3,max:20})
+            .withMessage("Last Name is required.")
+            .matches(/^[a-zA-Z]+$/)
+            .withMessage("Last Name must contain only alphabetic characters"),
+
+        body("password")
+            .isLength({min:8})
+            .withMessage("Password Must be at least 8 characters long.")
+            .matches(/\d/)
+            .withMessage("Password must contain at least one digit.")
+            .matches(/[!@#$%^(),.":{}|]/)
+            .withMessage('Password must contain at least one special character.')
+            .trim(),
     ];
 }
